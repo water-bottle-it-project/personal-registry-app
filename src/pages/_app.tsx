aa1403f657863ee8b29858152e83fe63b5320e08
Ian Chen <59730528+skyarrow2177@users.noreply.github.com>
2022-08-13 19:13:51 +1000

    WB-23: Add UI libraries (#1)
    WB-23: Add UI libraries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,47 @@
-import '@/styles/globals.css';
-
+import type { ColorScheme, MantineThemeOverride } from '@mantine/core';
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import { setCookies } from 'cookies-next';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type CustomAppProps = AppProps & {
+  initialColorScheme: ColorScheme;
+};
+
+function MyApp({ Component, pageProps, initialColorScheme }: CustomAppProps) {
+  // Dark mode support
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(initialColorScheme);
+
+  // Dark mode event handler
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookies('mantine-color-scheme', nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  // Global theme overrides
+  const themeOverride: MantineThemeOverride = {
+    colorScheme,
+    components: {
+      Paper: {
+        defaultProps: { p: 'md', pt: 2, shadow: 'xl', withBorder: true },
+      },
+    },
+  };
+
+  // Render top-level App component
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={themeOverride} withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
 
 export default MyApp;
