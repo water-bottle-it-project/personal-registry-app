a8803941cbf8d32df055ff996716f80369e4a48f
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-08-22 09:09:29 +1000

    Upgrade Mantine deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import { Container } from '@mantine/core';
 import type { NextPage } from 'next';
 
-import { ColorSchemeToggle } from '~components/app/ColorSchemeToggle';
-import { HeroBullets } from '~components/homepage/HeroOld';
-
 const Home: NextPage = () => {
   return (
     <>
-      <Container fluid>
-        <HeroBullets />
-      </Container>
+      <Container fluid />
     </>
   );
 };
