bce5292b34539bcecc3e24e3054736982a4e1532
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-08-21 17:43:36 +1000

    WB-48 created timeline card component using Mantine UI

---
+++ src/pages/timeline.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+
+import { TimelineCard } from '~components/app/TimelineCard';
+
+const Timeline: NextPage = () => {
+  return (
+    <>
+      <div>Hello timeline</div>
+      <TimelineCard />
+    </>
+  );
+};
+
+export default Timeline;
