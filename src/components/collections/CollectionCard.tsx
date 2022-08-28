c933ea0aae36fb2541a86de054d144ef30d35419
Sean Tan <79890362+seantanys@users.noreply.github.com>
2022-08-28 13:05:21 +1000

    WB-58 added backend router, and querying stuff (#16)
    Only one commit in this branch,  squashing it due to conflicts in the collections stuff

--- src/components/collections/CollectionCard.tsx
+++ src/components/collections/CollectionCard.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Card, Group, Space, Text, Title } from '@mantine/core';
+import { Anchor, Card, Container, createStyles, Group, Space, Text, Title } from '@mantine/core';
 import Link from 'next/link';
 
 interface CollectionCardProps {
@@ -10,37 +10,47 @@ interface CollectionCardProps {
 }
 
 export function CollectionCard(props: CollectionCardProps) {
+  const { classes } = useStyles();
+
   return (
-    <Card
-      p='md'
-      radius='md'
-      sx={theme => ({
-        fontSize: theme.fontSizes.lg,
-        width: 300,
-        backgroundColor: theme.colors[props.color][2],
-      })}
-      withBorder
-    >
-      <div>
-        <Title order={3}>{props.title}</Title>
-        <Space h='md' />
-        <Text>{props.description}</Text>
-      </div>
-      <Space h='xl' />
-      <div>
-        <Group position='apart'>
-          <Link href='/'>
-            <Anchor>
-              <Text>View {props.postCount} posts</Text>
-            </Anchor>
-          </Link>
-          <Link href='/'>
-            <Anchor>
-              <Text color='red'>Edit</Text>
-            </Anchor>
-          </Link>
-        </Group>
-      </div>
-    </Card>
+    <Container className={classes.wrapper}>
+      <Card
+        p='md'
+        radius='md'
+        sx={theme => ({
+          fontSize: theme.fontSizes.lg,
+          width: 300,
+          backgroundColor: theme.colors[props.color][2],
+        })}
+        withBorder
+      >
+        <div>
+          <Title order={3}>{props.title}</Title>
+          <Space h='md' />
+          <Text>{props.description}</Text>
+        </div>
+        <Space h='xl' />
+        <div>
+          <Group position='apart'>
+            <Link href='/'>
+              <Anchor>
+                <Text>View {props.postCount} posts</Text>
+              </Anchor>
+            </Link>
+            <Link href='/'>
+              <Anchor>
+                <Text color='red'>Edit</Text>
+              </Anchor>
+            </Link>
+          </Group>
+        </div>
+      </Card>
+    </Container>
   );
 }
+
+const useStyles = createStyles(theme => ({
+  wrapper: {
+    justifyContent: 'center',
+  },
+}));
--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -4,14 +4,6 @@ import type { NextPage } from 'next';
 import { CollectionIndex } from '~components/collections/CollectionIndex';
 
 const Collections: NextPage = () => {
-  const collectionProps = {
-    title: 'Gadgets',
-    description: 'Go go gadget!!!',
-    userId: '1234',
-    color: 'blue',
-    postCount: 3,
-  };
-
   return (
     <Container px={30} size={1440}>
       <CollectionIndex />
