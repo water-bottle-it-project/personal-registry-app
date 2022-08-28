73f20936526ddd409fc88654241d0d19d0b53f26
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-08-28 10:56:28 +1000

    added an overlay box

--- src/pages/images.tsx
+++ src/pages/images.tsx
@@ -2,12 +2,13 @@ import { Container } from '@mantine/core';
 import type { NextPage } from 'next';
 
 import { ImagesIndex } from '~components/image/ImageIndex';
+import { ImageOverlay } from '~components/image/ImageOverlay';
 
 const Image: NextPage = () => {
   return (
     <Container px={30} size={1500}>
       <div>All Images hehe</div>
-      <ImagesIndex />
+      <ImageOverlay />
     </Container>
   );
 };
