485dfab9decbff5083883ae7b1202f11f0c28d96
seantanys <79890362+seantanys@users.noreply.github.com>
2022-08-30 23:41:40 +1000

    protected collections route

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -1,14 +1,19 @@
 import { Container } from '@mantine/core';
 import type { NextPage } from 'next';
+import { NextSeo } from 'next-seo';
 
+import { withAuthedPage } from '~clientUtils/authHooks';
 import { CollectionIndex } from '~components/collections/CollectionIndex';
 
-const Collections: NextPage = () => {
+const Collections = () => {
   return (
-    <Container px={30} size={1440}>
-      <CollectionIndex />
-    </Container>
+    <>
+      <NextSeo description='Create a collection in my personal registry' title='Collections' />
+      <Container px={30} size={1440}>
+        <CollectionIndex />
+      </Container>
+    </>
   );
 };
 
-export default Collections;
+export default withAuthedPage(Collections);
