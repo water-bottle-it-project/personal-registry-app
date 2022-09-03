afd84c154b633c9e0644496ef58dc5215ce2735e
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-04 09:51:46 +1000

    WB-82: verify auth middleware works

--- src/server/routers/debugAuthed.ts
+++ src/server/routers/debugAuthed.ts
@@ -2,6 +2,7 @@ import { createProtectedDbRouter } from '~server/createProtectedDbRouter';
 
 const debugAuthedRouter = createProtectedDbRouter().query('getAuthedDebug', {
   resolve({ ctx }) {
+    // console.log(`ctx.userId: ${ctx.userId}`);
     return {
       result: 12345,
     };
