8f2c312a3ea8f4409bb988cd343cb8a2fdedf689
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-04 16:45:26 +1000

    WB-82: Add tooltip for colour picker

--- src/components/collection/ColorControl.tsx
+++ src/components/collection/ColorControl.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon, ColorSwatch, Group, Input, useMantineTheme } from '@mantine/core';
+import { CheckIcon, ColorSwatch, Group, Input, Tooltip, useMantineTheme } from '@mantine/core';
 import { upperFirst } from '@mantine/hooks';
 
 interface ColorControlProps {
@@ -11,23 +11,24 @@ export function ColorControl({ value, label, onChange, ...others }: ColorControl
   const theme = useMantineTheme();
 
   const colors = Object.keys(theme.colors).map(color => (
-    <ColorSwatch
-      color={theme.colorScheme === 'dark' ? theme.colors[color][7] : theme.colors[color][5]}
-      component='button'
-      key={color}
-      onClick={() => onChange(color)}
-      radius='xl'
-      size={48}
-      sx={{
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: theme.colorScheme === 'dark' ? theme.colors[color][2] : theme.white,
-      }}
-    >
-      {value === color && <CheckIcon height={12} width={12} />}
-    </ColorSwatch>
+    <Tooltip key={color} label={color}>
+      <ColorSwatch
+        color={theme.colorScheme === 'dark' ? theme.colors[color][7] : theme.colors[color][5]}
+        component='button'
+        onClick={() => onChange(color)}
+        radius='xl'
+        size={48}
+        sx={{
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: theme.colorScheme === 'dark' ? theme.colors[color][2] : theme.white,
+        }}
+      >
+        {value === color && <CheckIcon height={12} width={12} />}
+      </ColorSwatch>
+    </Tooltip>
   ));
 
   return (
