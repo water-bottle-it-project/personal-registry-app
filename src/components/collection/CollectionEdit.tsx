eaa6a51cafb6a7b5524f0e9c808931eb5b7884d6
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-04 16:41:32 +1000

    WB-82: Working edit form for variable id with retrieval of previous value

--- src/components/collection/CollectionEdit.tsx
+++ src/components/collection/CollectionEdit.tsx
@@ -1,9 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Stack, Textarea, TextInput } from '@mantine/core';
+import { Button, Stack, Text, Textarea, TextInput } from '@mantine/core';
 import { Controller, useForm } from 'react-hook-form';
 
 import { trpcClient } from '~clientUtils/trpcClient';
 import { ColorControl } from '~components/collection/ColorControl';
+import type { collectionT } from '~types/collection/collection';
 import type { collectionIdOnlyT } from '~types/collection/collectionIdOnly';
 import type { collectionOmitIdT } from '~types/collection/collectionOmitId';
 import { collectionOmitIdZ } from '~types/collection/collectionOmitId';
@@ -11,6 +12,10 @@ import { collectionOmitIdZ } from '~types/collection/collectionOmitId';
 export function CollectionEdit(props: collectionIdOnlyT) {
   const mutation = trpcClient.useMutation(['collection.UpdateCollection']);
   const trpcUtils = trpcClient.useContext();
+  const { data, isLoading, isError, error } = trpcClient.useQuery([
+    'collection.GetCollection',
+    { _id: props._id },
+  ]);
 
   function handleCollectionEdit({ title, color, description }: collectionOmitIdT) {
     mutation.mutate(
@@ -26,6 +31,34 @@ export function CollectionEdit(props: collectionIdOnlyT) {
     );
   }
 
+  if (isLoading || !data?.collection) {
+    return <Text>Loading collection details...</Text>;
+  }
+
+  if (isError) {
+    return <Text>Error loading collection details: {error?.message}</Text>;
+  }
+
+  return (
+    <CollectionEditForm collection={data.collection} handleCollectionEdit={handleCollectionEdit} />
+  );
+}
+
+interface CollectionEditFormProps {
+  collection: collectionT;
+  handleCollectionEdit: ({ title, color, description }: collectionOmitIdT) => void;
+}
+
+/**
+ * Only render form if collection is known to exist
+ * Note this takes a copy of the data (does not reflect background changes)
+ * https://tkdodo.eu/blog/react-query-and-forms
+ * (I couldn't get the background controlled form to work properly yet).
+ * @param collection
+ * @param handleCollectionEdit
+ * @constructor
+ */
+function CollectionEditForm({ collection, handleCollectionEdit }: CollectionEditFormProps) {
   const {
     register,
     handleSubmit,
@@ -33,6 +66,7 @@ export function CollectionEdit(props: collectionIdOnlyT) {
     formState: { errors },
   } = useForm<collectionOmitIdT>({
     resolver: zodResolver(collectionOmitIdZ),
+    defaultValues: collection,
   });
 
   return (
