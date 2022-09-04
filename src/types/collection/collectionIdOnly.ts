2a987073a3e296efa179015217e834d85e529306
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-04 10:05:52 +1000

    WB-82: rename collection ids

---
+++ src/types/collection/collectionIdOnly.ts
@@ -0,0 +1,9 @@
+import type { z } from 'zod';
+
+import { collectionZ } from '~types/collection/collection';
+const collectionIdOnlyZ = collectionZ.pick({ _id: true });
+
+type collectionIdOnlyT = z.infer<typeof collectionIdOnlyZ>;
+
+export type { collectionIdOnlyT };
+export { collectionIdOnlyZ };
