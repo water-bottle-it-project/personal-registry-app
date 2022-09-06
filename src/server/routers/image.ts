a549c36e01a8228dabbe94dda15e254ca71119b8
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-09-06 20:18:04 +1000

    made the image router protected, and response types for router with the listImages method

--- src/server/routers/image.ts
+++ src/server/routers/image.ts
@@ -1,49 +1,20 @@
 import * as trpc from '@trpc/server';
 
-import { createUnauthedDbRouter } from '~server/createUnauthedDbRouter';
+import { createProtectedDbRouter } from '~server/createProtectedDbRouter';
 import { Photo } from '~server/models/photo';
+import type { imageT } from '~types/image/image';
+import { imageZ } from '~types/image/image';
 
 /**
  * Router for debug operations
  */
-const imagesRouter = createUnauthedDbRouter()
-  .query('listImages', {
-    async resolve() {
-      const photos = await Photo.find();
-      return {
-        photos,
-      };
-    },
-  })
-
-  .mutation('AddImage', {
-    async resolve() {
-      const newImage = await Photo.create({
-        caption: 'sus',
-        url: 'gs://register-app-40207.appspot.com/debug1/childhood.jpeg',
-        userId: 'debug1',
-        memoryId: 'mem123',
-        memoryDate: 'ytdy',
-      });
-      return {
-        user: newImage,
-      };
-    },
-  })
-
-  .mutation('removeImage', {
-    async resolve() {
-      const result = await Photo.deleteOne({ caption: 'sus' });
-      if (result.deletedCount == 0) {
-        throw new trpc.TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'User _id not found.',
-        });
-      }
-      return {
-        success: true,
-      };
-    },
-  });
+const imagesRouter = createProtectedDbRouter().query('listImages', {
+  async resolve({ ctx }) {
+    const photos: imageT[] = await Photo.find({ userId: ctx.userId });
+    return {
+      photos,
+    };
+  },
+});
 
 export { imagesRouter };
