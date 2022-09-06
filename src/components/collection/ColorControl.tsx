water-bottle-it-project/personal-registry-app

commit 7ae0d29d1fec006c27fa4059fed966067d663316

WB-82: small tweaks

src/components/collection/ColorControl.tsx
@@ -11,7 +11,7 @@ export function ColorControl({ value, label, onChange, ...others }: ColorControl
   const theme = useMantineTheme();
 
   const colors = Object.keys(theme.colors).map(color => (
-    <Tooltip key={color} label={color.charAt(0).toUpperCase() + color.slice(1)}>
+    <Tooltip key={color} label={upperFirst(label)}>
       <ColorSwatch
         color={theme.colorScheme === 'dark' ? theme.colors[color][7] : theme.colors[color][5]}
         component='button'
@@ -43,5 +43,3 @@ export function ColorControl({ value, label, onChange, ...others }: ColorControl
     </Input.Wrapper>
   );
 }
-
-ColorControl.initialValue = 'blue';

src/components/collection/CollectionCard.tsx
@@ -27,7 +27,7 @@ export function CollectionCard({ _id, description, title, color }: CollectionCar
         <Group position='apart'>
           <Link href='/' passHref>
             <Anchor component='a'>
-              <Text>View posts</Text>
+              <Text>View memories</Text>
             </Anchor>
           </Link>
           <Link href={`/collections?edit=${_id}`} passHref>

src/components/collection/CollectionEdit.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Group, Space, Stack, Text, Textarea, TextInput } from '@mantine/core';
 import { useScrollLock } from '@mantine/hooks';
-import { closeAllModals, openConfirmModal } from '@mantine/modals';
+import { openConfirmModal } from '@mantine/modals';
 import { showNotification } from '@mantine/notifications';
 import { IconCheck, IconDeviceFloppy, IconRotateClockwise2, IconTrash, IconX } from '@tabler/icons';
 import { useRouter } from 'next/router';
@@ -113,6 +113,7 @@ interface CollectionEditFormProps {
  * TextArea).
  * @param collection
  * @param handleCollectionEdit
+ * @param handleCollectionDelete
  * @constructor
  */
 function CollectionEditForm({

src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { ColorScheme } from '@mantine/core';
 import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
-import { ModalsProvider } from '@mantine/modals';
 import { useLocalStorage } from '@mantine/hooks';
+import { ModalsProvider } from '@mantine/modals';
 import { NotificationsProvider } from '@mantine/notifications';
 import { withTRPC } from '@trpc/next';
 import type { AppProps } from 'next/app';

commit 4c511560b46e816a3b2778c8e48795095b86b204

Fix colour labels regression from 7ae0d29d

src/components/collection/ColorControl.tsx
@@ -13,7 +13,7 @@ export function ColorControl({ value, label, onChange, inputRef, ...others }: Co
   const theme = useMantineTheme();
 
   const colors = Object.keys(theme.colors).map(color => (
-    <Tooltip key={color} label={upperFirst(label)}>
+    <Tooltip key={color} label={upperFirst(color)}>
       <ColorSwatch
         color={theme.colorScheme === 'dark' ? theme.colors[color][7] : theme.colors[color][5]}
         component='button'