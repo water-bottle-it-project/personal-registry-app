0d9d698c26755195563253f4e01c974d9d1e37bd
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-07 12:41:21 +1000

    Clear data on signout to prevent flash of previous data

--- src/components/app/AppUserMenu.tsx
+++ src/components/app/AppUserMenu.tsx
@@ -2,13 +2,21 @@ import { Button } from '@mantine/core';
 import { useAuthUser } from 'next-firebase-auth';
 
 import { withAuthComponent } from '~clientUtils/authHooks';
+import { trpcClient } from '~clientUtils/trpcClient';
 import { LinkButton } from '~components/util/LinkButton';
 
 function AppUserMenuBase() {
   const { id, signOut } = useAuthUser();
 
+  const trpcUtils = trpcClient.useContext();
+
+  async function handleSignOut() {
+    await signOut();
+    await trpcUtils.queryClient.getQueryCache().clear();
+  }
+
   return id ? (
-    <Button color='orange' onClick={signOut} variant='light'>
+    <Button color='orange' onClick={handleSignOut} variant='light'>
       Sign out
     </Button>
   ) : (
