c07acd2cea6d82ce74b9c3daa59298d2e0ee79c1
seantanys <79890362+seantanys@users.noreply.github.com>
2022-09-07 21:53:54 +1000

    WB-47 attempt to change chip color using styles api
    had to use !important, but not a good practice.

--- src/components/image/ImagesHeader.tsx
+++ src/components/image/ImagesHeader.tsx
@@ -1,25 +1,64 @@
-import { Chip, Container, Group, Space, Text } from '@mantine/core';
+import { Chip, Container, Group, Space, Text, useMantineTheme } from '@mantine/core';
 
 import { SearchBar } from '~components/util/searchBar';
 
 export function ImagesHeader() {
+  const theme = useMantineTheme();
   return (
     <>
       <Container size='xl'>
         <Space h='xl' />
         <SearchBar
           searchFilters={['Title', 'Description', 'Collection']}
-          searchPlaceHolder='search for an image'
+          searchPlaceHolder='Search for an image'
           searchTitle='All Images'
         />
         <Space h='xl' />
         <Text size='md'>Collections</Text>
         <Space h='xs' />
         <Group>
-          <Chip color='purple'>Indigo</Chip>
-          <Chip color='teal'>Awesome chip</Chip>
-          <Chip color='purple'>Indigo</Chip>
-          <Chip color='purple'>cyan</Chip>
+          {/* Only way I know how to change chip color is by using !imporant, but bad practice
+          Not too sure of any other way, as the gray seems to be programmed to override anything else */}
+          <Chip
+            color='red'
+            radius='md'
+            styles={{
+              label: { backgroundColor: `${theme.colors.red[2]} !important` },
+            }}
+            variant='filled'
+          >
+            Gadgets
+          </Chip>
+          <Chip
+            color='teal'
+            radius='md'
+            styles={{
+              label: { backgroundColor: `${theme.colors.teal[2]} !important` },
+            }}
+            variant='filled'
+          >
+            Friends
+          </Chip>
+          <Chip
+            color='indigo'
+            radius='md'
+            styles={{
+              label: { backgroundColor: `${theme.colors.indigo[2]} !important` },
+            }}
+            variant='filled'
+          >
+            Family
+          </Chip>
+          <Chip
+            color='gray'
+            radius='md'
+            styles={{
+              label: { backgroundColor: `${theme.colors.gray[2]} !important` },
+            }}
+            variant='filled'
+          >
+            Travel
+          </Chip>
         </Group>
         <Space h='xl' />
       </Container>
