442cffed841e16c8e511d2205b2ec01c3bb5e3e3
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-07 19:32:50 +1000

    WB-63: Add reset to create form

--- src/components/create/CreateFormTop.tsx
+++ src/components/create/CreateFormTop.tsx
@@ -1,12 +1,15 @@
 import { Button, Group, Title } from '@mantine/core';
 import { IconPlus, IconRotateClockwise2 } from '@tabler/icons';
+import type { UseFormReturn } from 'react-hook-form';
 
-export function CreateFormTop() {
+import type { memoryFormOmitId } from '~types/memory/memoryForm';
+
+export function CreateFormTop({ reset }: UseFormReturn<memoryFormOmitId>) {
   return (
     <Group position='apart'>
       <Title>Create a memory</Title>
       <Group position='right'>
-        <Button rightIcon={<IconRotateClockwise2 />} variant='default'>
+        <Button onClick={() => reset()} rightIcon={<IconRotateClockwise2 />} variant='default'>
           Reset
         </Button>
         <Button rightIcon={<IconPlus />} type='submit'>
