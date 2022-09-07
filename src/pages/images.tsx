1abf91f5c252d531c2cb5c0d4b7f9cbc166efee1
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-09-07 11:14:49 +1000

    added a search bar component in utiils

--- src/pages/images.tsx
+++ src/pages/images.tsx
@@ -2,10 +2,12 @@ import { Container } from '@mantine/core';
 import type { NextPage } from 'next';
 
 import { ImagesIndex } from '~components/image/ImageIndex';
+import { SearchBar } from '~components/util/searchBar';
 
 const Images: NextPage = () => {
   return (
     <Container px={30} size={1500}>
+      <SearchBar />
       <div>All Images hehe</div>
       <ImagesIndex />
     </Container>
