c9d325f3b3ed468ee2e9c66e1f52ff5f95ebe3aa
seantanys <79890362+seantanys@users.noreply.github.com>
2022-09-10 21:00:49 +1000

    WB-68 created front end components for header

--- src/components/memory/MemoryHeader.tsx
+++ src/components/memory/MemoryHeader.tsx
@@ -1,22 +1,65 @@
-import { Box } from '@mantine/core';
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Group,
+  Select,
+  Space,
+  Text,
+  Title,
+} from '@mantine/core';
+import { IconDownload } from '@tabler/icons';
 
 export function MemoryHeader() {
   return (
     <Box
       sx={theme => ({
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        textAlign: 'center',
         padding: theme.spacing.xl,
-        borderRadius: theme.radius.md,
-        cursor: 'pointer',
-
-        '&:hover': {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-        },
       })}
     >
-      Hello
+      <Container size='xl'>
+        <Group>
+          <Title order={1}>Memory Name</Title>
+          <Button variant='outline'>Edit</Button>
+        </Group>
+        <Space h='md' />
+        <Title order={4}>Memory creation date</Title>
+        <Text>1 August 2022</Text>
+        <Space h='sm' />
+        <Title order={4}>Memory range</Title>
+        <Text>1 July 2022 to 1 August 2022</Text>
+        <Space h='xl' />
+        <Group position='apart'>
+          <Group>
+            <Title order={4}>Sort by</Title>
+            <Select
+              placeholder='Default'
+              data={[
+                { value: 'newToOld', label: 'Newest to Oldest' },
+                { value: 'oldToNew', label: 'Oldest to Newest' },
+              ]}
+            />
+          </Group>
+          <div>
+            <Button leftIcon={<IconDownload />} variant='outline'>
+              Download
+            </Button>
+          </div>
+        </Group>
+        <Space h='xl' />
+        <Title order={4}>Collections</Title>
+        <Space h='sm' />
+        <Group>
+          <Badge radius='xs'>Gadgets</Badge>
+          <Badge radius='xs'>Gadgets</Badge>
+          <Badge radius='xs'>Gadgets</Badge>
+          <Badge radius='xs'>Gadgets</Badge>
+          <Badge radius='xs'>Gadgets</Badge>
+        </Group>
+      </Container>
     </Box>
   );
 }
