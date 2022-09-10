af6a2ae6898973a1f00e06d1882adf0fa66ac423
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-10 14:15:05 +1000

    WB-63: reset with cleanup of thumbnails to prevent potential memory leak

--- src/components/create/CreateFormTop.tsx
+++ src/components/create/CreateFormTop.tsx
@@ -4,12 +4,20 @@ import type { UseFormReturn } from 'react-hook-form';
 
 import type { memoryCreateFormT } from '~types/memory/memoryForm';
 
-export function CreateFormTop({ reset }: UseFormReturn<memoryCreateFormT>) {
+export function CreateFormTop({ reset, getValues }: UseFormReturn<memoryCreateFormT>) {
+  function resetWithCleanup() {
+    const photos = getValues('photos');
+    reset();
+    photos.forEach(p => {
+      URL.revokeObjectURL(p._thumbnail);
+    });
+  }
+
   return (
     <Group position='apart'>
       <Title>Create a memory</Title>
       <Group position='right'>
-        <Button onClick={() => reset()} rightIcon={<IconRotateClockwise2 />} variant='default'>
+        <Button onClick={resetWithCleanup} rightIcon={<IconRotateClockwise2 />} variant='default'>
           Reset
         </Button>
         <Button rightIcon={<IconPlus />} type='submit'>
