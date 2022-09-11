2c51bafcf7ce75b8981e7cc457392f72b006682c
nboclzz <68681955+nboclzz@users.noreply.github.com>
2022-09-11 14:01:44 +1000

    move fetching to component

--- src/components/memory/MemoryIndex.tsx
+++ src/components/memory/MemoryIndex.tsx
@@ -1,17 +1,35 @@
 import { Container } from '@mantine/core';
 
-import type { memoryT } from '~types/memory/memory';
-
 import { MemoryHeader } from './MemoryHeader';
+import { trpcClient } from '~clientUtils/trpcClient';
 
-interface MemoryIndexProps extends memoryT {
-  imageCount?: number;
+interface MemoryIndexProps {
+  _id: string;
 }
 
 export function MemoryIndex(props: MemoryIndexProps) {
+  const { data, isLoadingError, isLoading } = trpcClient.useQuery([
+    'memory.GetMemory',
+    { _id: props._id },
+  ]);
+  if (isLoading || !data?.memory) {
+    return <div>Loading...</div>;
+  }
+  if (isLoadingError) {
+    return <div>Error loading memory</div>;
+  }
+  const { title, description, firstDate, lastDate, photos, collections } = data.memory;
   return (
     <>
-      <MemoryHeader />
+      <MemoryHeader
+        _id={props._id}
+        title={title}
+        collections={collections}
+        photos={photos}
+        firstDate={firstDate}
+        lastDate={lastDate}
+        description={description}
+      />
       <Container size='xl'>{/* Insert images here */}</Container>
     </>
   );
--- src/pages/memory/[id].tsx
+++ src/pages/memory/[id].tsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
 
 import { withAuthedPage } from '~clientUtils/authHooks';
-import { trpcClient } from '~clientUtils/trpcClient';
 import { MemoryIndex } from '~components/memory/MemoryIndex';
 
 function Memory() {
@@ -10,30 +9,10 @@ function Memory() {
   if (typeof id !== 'string') {
     return null;
   }
-  const { data, isLoadingError, isLoading, error } = trpcClient.useQuery([
-    'memory.GetMemory',
-    { _id: id },
-  ]);
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (isLoadingError) {
-    return <div>Error loading memory: {error?.message}</div>;
-  }
-  const { title, description, firstDate, lastDate, photos, collections } = data?.memory || {};
-  collections && console.log(collections);
+
   return (
     <>
-      <MemoryIndex />
-      <h1>{title}</h1>
-      <p>Description: {description}</p>
-      <p>Collections:</p>
-      {collections && collections.map(c => <div>{c.collectionId}</div>)}
-
-      <p>Start Date: {firstDate && firstDate.toString()}</p>
-      <p>End Date: {lastDate && lastDate.toString()}</p>
-      <p>Photos:</p>
-      {photos && photos.map(c => <div>{c.photoId}</div>)}
+      <MemoryIndex _id={id} />
     </>
   );
 }
