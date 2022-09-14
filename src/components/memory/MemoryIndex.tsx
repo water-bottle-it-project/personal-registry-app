a9e0113a55f33a4fa47d2d08f0c8b30625e1f217
seantanys <79890362+seantanys@users.noreply.github.com>
2022-09-14 20:29:27 +1000

    WB-127 naive attempt at populating image grid

--- src/components/memory/MemoryIndex.tsx
+++ src/components/memory/MemoryIndex.tsx
@@ -1,14 +1,11 @@
-import { Container, Grid, Space } from '@mantine/core';
-import { useRouter } from 'next/router';
-import { Key } from 'react';
+import { Container, Grid, Space, Stack } from '@mantine/core';
 
 import { trpcClient } from '~clientUtils/trpcClient';
 import type { photoIdOnlyT } from '~types/photo/photo';
 
 import { MemoryHeader } from './MemoryHeader';
 import { MemoryImage } from './MemoryImage';
-import { MemoryImageGrid } from './MemoryImageGrid';
-import { MemoryImageGrid2 } from './MemoryImageGrid2';
+import { MemoryImage2 } from './MemoryImage2';
 
 interface MemoryIndexProps {
   _id: string;
@@ -27,6 +24,39 @@ export function MemoryIndex(props: MemoryIndexProps) {
   }
   const { title, collections, description, firstDate, lastDate, photos } = data.memory;
 
+  // What im trying to achieve:
+  // Distribute images on the masonry grid evenly
+  // My approach:
+  // Every nth photo in the photos array, switch columns and populate that column
+  // This is a very naive attempt and does not respect image order.
+  let colCount = 1;
+  const photoCol1: any[] = [];
+  const photoCol2: any[] = [];
+  const photoCol3: any[] = [];
+
+  for (let i = 0; i < photos.length; i++) {
+    if ((i - 1) % 2 == 0 && i !== 0) {
+      switch (colCount) {
+        case 1:
+          colCount = 2;
+          break;
+        case 2:
+          colCount = 3;
+          break;
+        case 3:
+          colCount = 1;
+          break;
+      }
+    }
+    if (colCount === 1) {
+      photoCol1.push(<MemoryImage2 _id={photos[i]._id} />);
+    } else if (colCount === 2) {
+      photoCol2.push(<MemoryImage2 _id={photos[i]._id} />);
+    } else {
+      photoCol3.push(<MemoryImage2 _id={photos[i]._id} />);
+    }
+  }
+
   const MemoryPhotos =
     photos && photos.map((c: photoIdOnlyT) => <MemoryImage _id={c._id} key={c._id} />);
 
@@ -42,18 +72,25 @@ export function MemoryIndex(props: MemoryIndexProps) {
         title={title}
       />
       {/* <Container size='xl'>{photos && photos.map(c => <div>{c._id}</div>)}</Container> */}
+      {/* <Space h='xl' />
+      {MemoryPhotos} */}
       <Space h='xl' />
-      {/* {MemoryPhotos} */}
       <Container size='xl'>
         <Grid>
-          <Grid.Col span={4}>
-            <MemoryImageGrid2 />
+          <Grid.Col lg={4} md={6}>
+            <Stack align='flex-start' justify='flex-start' spacing='xs'>
+              {photoCol1}
+            </Stack>
           </Grid.Col>
-          <Grid.Col span={4}>
-            <MemoryImageGrid />
+          <Grid.Col lg={4} md={6}>
+            <Stack align='flex-start' justify='flex-start' spacing='xs'>
+              {photoCol2}
+            </Stack>
           </Grid.Col>
-          <Grid.Col span={4}>
-            <MemoryImageGrid />
+          <Grid.Col lg={4} md={6}>
+            <Stack align='flex-start' justify='flex-start' spacing='xs'>
+              {photoCol3}
+            </Stack>
           </Grid.Col>
         </Grid>
       </Container>
