92f4156065eb7ba2a88c520cd46138b5fcbbd529
seantanys <79890362+seantanys@users.noreply.github.com>
2022-09-14 20:58:28 +1000

    WB-127 added caption on hover

--- src/components/memory/MemoryImage2.tsx
+++ src/components/memory/MemoryImage2.tsx
@@ -1,12 +1,15 @@
-import { Container, createStyles, Divider, Grid, Image, Space, Stack, Title } from '@mantine/core';
+import { createStyles, Image, Text } from '@mantine/core';
 import { useHover } from '@mantine/hooks';
+import { useState } from 'react';
 
 import { trpcClient } from '~clientUtils/trpcClient';
+import { ImageOverlayInfo } from '~components/image/ImageOverlayInfo';
 import type { photoIdOnlyT } from '~types/photo/photo';
 
 export function MemoryImage2(props: photoIdOnlyT) {
   const { hovered, ref } = useHover();
-  const { classes } = useStyles();
+  const [info, setInfo] = useState(false);
+  const { classes, cx } = useStyles();
   const { data, isLoadingError, isLoading } = trpcClient.useQuery([
     'images.getImage',
     { _id: props._id },
@@ -20,24 +23,44 @@ export function MemoryImage2(props: photoIdOnlyT) {
   }
 
   return (
-    <div ref={ref}>
-      <Image alt='' height='40%' src={data?.image.url} width='100%' />
+    <div onClick={() => setInfo(!info)} ref={ref}>
+      <Image
+        alt={data?.image.caption}
+        className={cx(classes.wrapper, { [classes.dimmed]: hovered === true })}
+        height='40%'
+        src={data?.image.url}
+        width='100%'
+      />
       {hovered && (
-        <Title className={classes.imgInfo} order={4}>
+        <Text className={classes.imgCaption} size='lg' weight={400}>
           {data?.image.caption}
-        </Title>
+        </Text>
+      )}
+      {info && (
+        <ImageOverlayInfo
+          _id={props._id}
+          caption={data?.image.caption}
+          url={data?.image.url}
+          userId={data?.image.userId}
+        />
       )}
     </div>
   );
 }
 
 const useStyles = createStyles(theme => ({
-  imgContainer: {
-    height: 'max-content',
+  wrapper: {
+    cursor: 'zoom-in',
+    transition: 'all 0.5s ease',
   },
-  imgInfo: {
+  imgCaption: {
     color: 'white',
     position: 'relative',
-    top: '-20px',
+    top: '-30px',
+    right: '-15px',
+    marginBottom: '-26px',
+  },
+  dimmed: {
+    filter: 'brightness(70%)',
   },
 }));
