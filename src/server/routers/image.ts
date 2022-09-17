e71c4bfff680c433f62bec7c7287c5039ded8be5
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-09-17 11:22:13 +1000

    WB-114 added imageIdOnly type

--- src/server/routers/image.ts
+++ src/server/routers/image.ts
@@ -1,13 +1,9 @@
-import * as trpc from '@trpc/server';
-import { resolve } from 'path';
-import { string } from 'zod';
-import { z } from 'zod';
+import { TRPCError } from '@trpc/server';
 
 import { createProtectedDbRouter } from '~server/createProtectedDbRouter';
 import { Photo } from '~server/models/photo';
-import { collectionZ } from '~types/collection/collection';
 import type { imageT } from '~types/image/image';
-
+import { imageIdOnlyZ } from '~types/image/image';
 /**
  * Router for debug operations
  */
@@ -22,9 +18,7 @@ const imagesRouter = createProtectedDbRouter()
   })
 
   .query('getImage', {
-    input: z.object({
-      _id: z.string().min(1),
-    }),
+    input: imageIdOnlyZ,
     async resolve({ ctx, input }) {
       const image = await Photo.findOne({
         _id: input._id,
@@ -32,7 +26,7 @@ const imagesRouter = createProtectedDbRouter()
       }); // findById('6308a49ab3b2b466112558ec');
 
       if (!image) {
-        throw new trpc.TRPCError({
+        throw new TRPCError({
           code: 'NOT_FOUND',
           message: 'Image not found by ID.',
         });
