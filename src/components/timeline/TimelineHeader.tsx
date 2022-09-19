cbbfaca579bfb1f7ec6c25aba4e77f21fb761149
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-19 16:21:59 +1000

    WB-129: Fix all memories view (excluding collections)

--- src/components/timeline/TimelineHeader.tsx
+++ src/components/timeline/TimelineHeader.tsx
@@ -1,11 +1,21 @@
-import { SearchBar } from '~components/util/searchBar';
+import { ActionIcon, Stack, TextInput, Title } from '@mantine/core';
+import { IconArrowRight, IconSearch } from '@tabler/icons';
 
 export function TimelineHeader() {
   return (
-    <SearchBar
-      searchFilters={['Title', 'Description']}
-      searchPlaceHolder='Search for a memory'
-      searchTitle='Your Timeline'
-    />
+    <Stack spacing='sm'>
+      <Title>Your memories</Title>
+      <TextInput
+        icon={<IconSearch size={18} stroke={1.5} />}
+        placeholder='Search your memories by title and description'
+        rightSection={
+          <ActionIcon color='indigo' size={32} variant='filled'>
+            <IconArrowRight size={18} stroke={1.5} />
+          </ActionIcon>
+        }
+        rightSectionWidth={42}
+        size='md'
+      />
+    </Stack>
   );
 }
