4dcd9cb148bc86d115bd3d1ce0b28204c5c2d6cb
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-09-21 14:58:43 +1000

    WB-132: Pagination component

--- src/components/timeline/TimelineIndex.tsx
+++ src/components/timeline/TimelineIndex.tsx
@@ -1,16 +1,18 @@
-import { Container, Space, Text } from '@mantine/core';
+import { Center, Container, Pagination, Space, Stack, Text } from '@mantine/core';
+import { useState } from 'react';
 
 import { trpcClient } from '~clientUtils/trpcClient';
+import { TimelineGrid } from '~components/timeline/TimelineGrid';
+import { TimelineHeader } from '~components/timeline/TimelineHeader';
 import { SkeletonGrid } from '~components/util/SkeletonGrid';
 
-import { TimelineGrid } from './TimelineGrid';
-import { TimelineHeader } from './TimelineHeader';
-
 export function TimelineIndex() {
+  const [page, setPage] = useState(1);
+
   // Lift query hook up to share search bar state with the memory results.
   const { data, isLoading, isLoadingError } = trpcClient.useQuery([
     'memory.GetMemoriesPaginated',
-    { page: 1 },
+    { page },
   ]);
 
   let contents;
@@ -19,7 +21,25 @@ export function TimelineIndex() {
   } else if (isLoadingError) {
     contents = <Text>Error loading memories. Try again later.</Text>;
   } else {
-    contents = <TimelineGrid memories={data.docs} />;
+    contents = (
+      <>
+        <Center>
+          <Pagination onChange={setPage} page={page} siblings={2} total={data.totalPages} />
+        </Center>
+        <Space h='sm' />
+        <TimelineGrid memories={data.docs} />
+        <Space h='xl' />
+        <Center>
+          <Stack align='center' spacing='xs'>
+            <Text color='dimmed' size='sm'>
+              Viewing memories {data.pagingCounter} to {data.pagingCounter + data.limit - 1}
+            </Text>
+            <Pagination onChange={setPage} page={page} siblings={2} total={data.totalPages} />
+          </Stack>
+        </Center>
+        <Space h='xl' />
+      </>
+    );
   }
 
   return (
