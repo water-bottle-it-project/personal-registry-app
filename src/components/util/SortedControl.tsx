789243eed2ad0d7d4efb1d914fb1effce4d268fb
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-10-20 21:52:17 +1100

    WB-113: Additional search sort messages

--- src/components/util/SortedControl.tsx
+++ src/components/util/SortedControl.tsx
@@ -1,4 +1,13 @@
-import { ActionIcon, Box, Center, Group, HoverCard, SegmentedControl, Text } from '@mantine/core';
+import {
+  ActionIcon,
+  Box,
+  Center,
+  Group,
+  HoverCard,
+  SegmentedControl,
+  Space,
+  Text,
+} from '@mantine/core';
 import { IconInfoCircle, IconSortAscending2, IconSortDescending2 } from '@tabler/icons';
 
 import type { sortOrderT } from '~types/util/sortOrderT';
@@ -6,9 +15,10 @@ import type { sortOrderT } from '~types/util/sortOrderT';
 interface SortedControlProps {
   defaultValue: sortOrderT;
   sortOrder: (value: ((prevState: sortOrderT) => sortOrderT) | sortOrderT) => void;
+  highlight?: boolean;
 }
 
-export function SortedControl({ defaultValue, sortOrder }: SortedControlProps) {
+export function SortedControl({ defaultValue, sortOrder, highlight = false }: SortedControlProps) {
   return (
     <Group>
       <SegmentedControl
@@ -41,11 +51,13 @@ export function SortedControl({ defaultValue, sortOrder }: SortedControlProps) {
       />
       <HoverCard shadow='md' width={280}>
         <HoverCard.Target>
-          <ActionIcon>
+          <ActionIcon color={highlight ? 'indigo' : undefined}>
             <IconInfoCircle />
           </ActionIcon>
         </HoverCard.Target>
         <HoverCard.Dropdown>
+          <Text size='sm'>Results are sorted by the end date of the memory.</Text>
+          <Space h='xs' />
           <Text size='sm'>
             Sorting is disabled when searching. Search results are ranked by string relevance.
           </Text>
