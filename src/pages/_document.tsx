bb3facad4b7482d615b7726c692ad139e95f053e
Ian Chen <59730528+SkyArrow2177@users.noreply.github.com>
2022-10-21 12:25:24 +1100

    WB-134: Add meta tags to document for iOS, Android etc.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,13 @@ export default class _Document extends Document {
             href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap'
             rel='stylesheet'
           />
+          <link href='/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
+          <link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
+          <link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
+          <link href='/site.webmanifest' rel='manifest' />
+          <link color='#5bbad5' href='/safari-pinned-tab.svg' rel='mask-icon' />
+          <meta content='#e7e7e7' name='msapplication-TileColor' />
+          <meta content='#e7e7e7' name='theme-color' />
         </Head>
         <body>
           <Main />
